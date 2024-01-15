e3ef5d068dfd09f023235593079f596b2591459c
talhaahmad316 <talhaahmad316@gmail.com>
2024-01-15 12:17:36 +0500

    Add Alerts

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -23,7 +23,7 @@
    <div class="col-md-12">
     <div class="card card-primary">
      <div class="card-header">
-      <h3 class="card-title">Edit Category</h3>
+      <h3 class="card-title">Edit Category : {{ $category->name}}</h3>
      </div>
     <form action="{{ url('category/'.$category->id)}}" method="POST" enctype="multipart/form-data">
     @csrf
