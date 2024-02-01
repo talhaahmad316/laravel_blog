15653d58c0f694438e7f1191f076d1a6fafd918c
talhaahmad316 <talhaahmad316@gmail.com>
2024-02-01 22:31:48 +0500

    Add Comments

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\UserMail;
 use Illuminate\Support\Facades\Auth;
@@ -32,7 +33,7 @@ class AuthController extends Controller
         $user=new User;
         $user->name=$request->name;
         $user->email=$request->email;
-        $user->password=$request->password;
+        $user->password = Hash::make($request->password); 
         $user->save();
         // Mail is used for send email 
         $detail=$request->all();
